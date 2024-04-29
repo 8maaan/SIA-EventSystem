f2d51be268e79d47318f4cb44529ae1d90fe6ed4
Dave Ondona <ondonadave5@gmail.com>
2024-04-29 18:09:09 +0800

    Made minor adjustments to the page.

--- client/src/Pages/CreateEvent.js
+++ client/src/Pages/CreateEvent.js
@@ -2,8 +2,8 @@ import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField } fro
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { addDoc, collection } from 'firebase/firestore';
-import React, { useState } from 'react';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { db } from "../Firebase/firebaseConfig";
 import ReusableAppBar from '../ReusableComponents/ReusableAppBar';
@@ -20,23 +20,55 @@ const CreateEvent = () => {
         eventDescription: ""
       });
 
+    const [departments, setDepartments] = useState(null);
+    const [fieldsFilled, setfieldsFilled] = useState(false);
+    const [focused, setFocused] = useState({
+        eventName: false,
+        eventLocation: false,
+        eventDescription: false
+    });
+
+    const getDepartments = async () => {
+
+        try {
+            const querySnapshot = await getDocs(collection(db, "departments"));
+            const departments = querySnapshot.docs.map(doc => {
+                const data = doc.data();
+                return {id: doc.id, ...data};
+            });
+
+            departments.sort((a, b) => {
+                const departmentA = a.department.toLowerCase();
+                const departmentB = b.department.toLowerCase();
+                if (departmentA < departmentB) {
+                    return -1;
+                }
+                if (departmentA > departmentB) {
+                    return 1;
+                }
+                return 0;
+            });
+                setDepartments(departments)
+        } catch (e) {
+            console.log(e.message)
+        }
+    }
+
+    const handleFocus = (field) => {
+        if (!focused[field]) {
+            setFocused(prevState => ({
+                ...prevState,
+                [field]: true
+            }));
+        }
+    };
+
     const handleSubmit = async () => {
 
         const eventDate = eventData.eventTimestamp ? eventData.eventTimestamp.toDate() : null;
 
-        if(!eventData.eventName.trim() || !eventData.eventTimestamp || 
-            !eventData.eventLocation.trim() || !eventData.eventDepartment.trim() || !eventData.eventDescription.trim()) {
-            alert('Please Enter All the Fields!');
-            return;
-        }
-
-        if(eventData.eventDescription.trim().split(/\s+/).length < 5) {
-            alert('Please enter a Event Description with atleast 5 words');
-            return;
-        }
-
-
         try {
+            console.log('hello')
             await addDoc(collection(db, 'event'), {
                 eventName: eventData.eventName,
                 eventTimestamp: eventDate,
@@ -44,13 +76,23 @@ const CreateEvent = () => {
                 eventDepartment: eventData.eventDepartment,
                 eventDescription: eventData.eventDescription
             })
-
+            console.log('hit!')
             navigateTo('/home');  
 
+        console.log("hello")
         } catch(e) {
             console.log(e.message);
         }
     }
+
+    useEffect(() => {
+        getDepartments();
+    }, [])
+
+    useEffect(() => {
+        const areAllFieldsFilled = Object.values(eventData).every(value => value !== "");
+        setfieldsFilled(areAllFieldsFilled);
+    }, [eventData])
 
     return (
         <div>
@@ -64,15 +106,18 @@ const CreateEvent = () => {
                 noValidate
                 autoComplete="off">
             <div>
-                <TextField id="standard-basic" label="Event Name" onChange={(event) => {setEventData(prevState => ({...prevState, eventName: event.target.value}))}}/>
+                <TextField id="standard-basic" label="Event Name" required error={focused.eventName && eventData.eventName.trim() === ""} 
+                    helperText={(focused.eventName && eventData.eventName.trim() === "") ? 'Please input a value' : ''}  onFocus={() => handleFocus('eventName')} 
+                    onChange={(event) => {setEventData(prevState => ({...prevState, eventName: event.target.value}))}}/>
+
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DateTimePicker label="Date & Time" onChange={(event) => {setEventData(prevState => ({...prevState, eventTimestamp: event}))}} />
+                        <DateTimePicker label="Date & Time *" onChange={(event) => {setEventData(prevState => ({...prevState, eventTimestamp: event}))}} />
                 </LocalizationProvider>
             </div>
             <div>
                 {/*TEXTFIELD IS OFF */}
                 <FormControl sx={{ m: 1.5, minWidth: 350}}>
-                <InputLabel id="demo-simple-select-label">Department</InputLabel>
+                <InputLabel id="demo-simple-select-label" required >Department</InputLabel>
                 <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
@@ -80,25 +125,26 @@ const CreateEvent = () => {
                     label="College Department"
                     onChange={(event) => {setEventData(prevState => ({...prevState, eventDepartment: event.target.value}))}}>
 
-
-                    {/* U CAN ADD A MAPPING FEATURE HERE SIMILAR TO LOADING EVENTS */}
-                    <MenuItem value={'College of Engineering and Architecture'}>College of Engineering and Architecture</MenuItem>
-                    <MenuItem value={'College of Management, Business and Accountancy'}>College of Management, Business and Accountancy</MenuItem>
-                    <MenuItem value={'College of Arts, Sciences, and Education'}>College of Arts, Sciences, and Education</MenuItem>
-                    <MenuItem value={'College of Nursing and Allied Health Sciences'}>College of Nursing and Allied Health Sciences</MenuItem>
-                    <MenuItem value={'College of Computer Studies'}>College of Computer Studies</MenuItem>
-                    <MenuItem value={'College of Criminal Justice'}>College of Criminal Justice</MenuItem>
+                    {departments && departments.map(department => (
+                        <MenuItem key={department.id} value={department.department}>{department.department}</MenuItem>
+                    ))}
                 </Select>
                 </FormControl>
-                <TextField id="standard-basic" label="Location" onChange={(event) => {setEventData(prevState => ({...prevState, eventLocation: event.target.value}))}}/>
+                <TextField id="standard-basic" label="Location" required error={focused.eventLocation && eventData.eventLocation.trim() === ""} 
+                    helperText={(focused.eventLocation && eventData.eventLocation.trim() === "") ? 'Please input a value' : ''}  onFocus={() => handleFocus('eventLocation')} 
+                    onChange={(event) => {setEventData(prevState => ({...prevState, eventLocation: event.target.value}))}}/>
+
             </div>
             <div>
-                <TextField id="standard-basic" label="Description" multiline rows={4} maxRows={8} onChange={(event) => {setEventData(prevState => ({...prevState, eventDescription: event.target.value}))}}/>
+                <TextField id="standard-basic" label="Description" required error={focused.eventDescription && eventData.eventDescription.trim() === ""} 
+                    helperText={(focused.eventDescription && eventData.eventDescription.trim() === "") ? 'Please input a value' : ''}  onFocus={() => handleFocus('eventDescription')} 
+                    multiline rows={4} maxRows={8} onChange={(event) => {setEventData(prevState => ({...prevState, eventDescription: event.target.value}))}}/>
+
             </div>
             <br></br>
             </Box>
             <div>
-                <Button variant='contained' size='large' color='success' onClick={handleSubmit}>Submit</Button>
+                <Button variant='contained' disabled={!fieldsFilled} size='large' color='success' type='submit' onClick={handleSubmit}>Submit</Button>
             </div>
         </div>
     )
